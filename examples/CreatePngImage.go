987406708ec476d9ff5851c3674f49336dc69926
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2020-12-18 23:06:26 +0000

    Slightly improved example code

--- examples/CreatePngImage.go
+++ examples/CreatePngImage.go
@@ -11,6 +11,7 @@ import (
 
 func main() {
 
+	// Define a colour palette for easier management of colours later
 	palette := map[string]color.Color{
 		"black":  color.RGBA{0, 0, 0, 255},
 		"white":  color.RGBA{255, 255, 255, 255},
@@ -25,16 +26,19 @@ func main() {
 
 	// Create a plane
 	plane := p.Plane{}
-
+	// ...and init it to the right size
 	plane.Init(
 		[2]int{512, 512},
 	)
-
+	// Get the size of the plane
+	// Technically unnecessary if we keep track of it above but left here for demo purposes
 	planeDimensions := plane.GetDimensions()
-
+	// Create the image. We will add pixels later
 	img := image.NewRGBA(image.Rect(0, 0, planeDimensions[0], planeDimensions[1]))
 
-	// Draw a square
+	// Drawing a square
+
+	// The points of a square
 	square := []p.Point{
 		p.Point{X: 10, Y: 10},
 		p.Point{X: 50, Y: 10},
@@ -42,11 +46,29 @@ func main() {
 		p.Point{X: 10, Y: 50},
 		p.Point{X: 10, Y: 10},
 	}
+	// Join them
 	points := plane.JoinPoints(square)
+	// Add them to the plane (non-strict so no need to check error)
+	_ = plane.WritePoints(points, false)
 
-	plane.Flip(1) // We're about to draw the points onto an image which has a reversed Y axis, so flip to account for this
+	// Drawing a triangle
 
-	for _, point := range points {
+	// The points of the triangle
+	triangle := []p.Point{
+		p.Point{X: 150, Y: 190},
+		p.Point{X: 80, Y: 70},
+		p.Point{X: 220, Y: 70},
+		p.Point{X: 150, Y: 190},
+	}
+	// Join the points
+	points = plane.JoinPoints(triangle)
+	// Add them to the plane (non-strict so no need to check error)
+	_ = plane.WritePoints(points, false)
+
+	plane.Flip(1) // We're about to draw the points onto an image which has a reversed Y axis, so flip along the Y axis to account for this
+
+	// Fetch the points from the plane and draw them as pixels on the image
+	for _, point := range plane.ReadPoints() {
 		img.Set(point.X, point.Y, palette["red"])
 	}
 
