df320b767eb718733b00e1b7573396c5e83d3c87
TR-SLimey <37966924+TR-SLimey@users.noreply.github.com>
2020-12-31 17:22:54 +0000

    Made set implementation thread-safe

--- sets.go
+++ sets.go
@@ -1,11 +1,15 @@
 package gopoints
 
+import "sync"
+
 // A simple set implementation for 2D points as Go does not provide one. Uses map keys as the storage
 // as they do not repeat and are unordered. The value used is a blank struct as it causes little
 // to no overead due to its 0-byte size.
 type PointSet struct {
 	// The actual data-storage part
-	data map[Point]struct{}
+	data sync.Map
+	// Keep track of whether the map has been initialised
+	dataPostInit bool
 }
 
 // Init initialises the set by creating the map which stores the data. This is called implicitly
@@ -13,8 +17,9 @@ type PointSet struct {
 // the set if called multiple times.
 func (set *PointSet) Init() {
 	// Only if the set is not already initialised in order to avoid accidentally removing data
-	if set.data == nil {
-		set.data = make(map[Point]struct{})
+	if !set.dataPostInit {
+		set.dataPostInit = true
+		set.data = sync.Map{}
 	}
 }
 
@@ -23,7 +28,7 @@ func (set *PointSet) Add(point Point) {
 	// Automatically init the set if it's not initialised yet (check handled by Init())
 	set.Init()
 
-	set.data[point] = struct{}{}
+	set.data.Store(point, struct{}{})
 }
 
 // AddArray works much like Add but accepts a slice of points (poor naming) and adds each element of it to the set
@@ -41,7 +46,7 @@ func (set *PointSet) Remove(point Point) {
 	// Automatically init the set if it's not initialised yet (check handled by Init())
 	set.Init()
 
-	delete(set.data, point)
+	set.data.Delete(point)
 }
 
 // RemoveArray works much like Remove but accepts a slice of points (poor naming) and removes each element of it from the set
@@ -55,11 +60,11 @@ func (set *PointSet) RemoveArray(pointArray []Point) {
 }
 
 // CheckFor checks if a given point is in the set
-func (set PointSet) CheckFor(point Point) bool {
+func (set *PointSet) CheckFor(point Point) bool {
 	// Automatically init the set if it's not initialised yet (check handled by Init())
 	set.Init()
 
-	_, ok := set.data[point]
+	_, ok := set.data.Load(point)
 	return ok
 }
 
@@ -74,14 +79,15 @@ func (set *PointSet) CheckForAll(points []Point) bool {
 }
 
 // AsArray returns the contents of the set as a slice
-func (set PointSet) AsArray() []Point {
+func (set *PointSet) AsArray() []Point {
 	// Automatically init the set if it's not initialised yet (check handled by Init())
 	set.Init()
 
 	// Convert the set to an array for easy access
 	arr := []Point{}
-	for point := range set.data {
-		arr = append(arr, point)
-	}
+	set.data.Range(func(key, value interface{}) bool {
+		arr = append(arr, key.(Point))
+		return true
+	})
 	return arr
 }
